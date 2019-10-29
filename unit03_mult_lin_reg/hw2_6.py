b4d5b7905fe79bc006dcf790456a4c925eb26031
mkarroqe@gmail.com <mkarroqe@gmail.com>
2019-10-29 09:44:45 -0400

    nb updates

--- unit03_mult_lin_reg/hw2_6.py
+++ unit03_mult_lin_reg/hw2_6.py
@@ -8,8 +8,8 @@ X = np.random.rand(nx,p)
 Y = np.random.rand(ny,p)
 
 # Computing the distances in two lines.  No for loop!
-DXY = X[:, None, :] - Y[None, :, :]
-d = np.sum(DXY**2, axis = 2)
+dists = x[:, None, :] - y[None, :, :]
+result = np.sum(dists**2, axis = 2)
 
 def loop(X, beta):
 	n = X.shape[0]
@@ -23,9 +23,18 @@ beta = np.matrix('1 2 3')
 
 n = X.shape[0]
 yhat = np.zeros(n)
+
 yhat = beta[0]*X[:,0] + beta[1]*X[:,1] + beta[2]*X[:,1]*X[:,2]
+
 print(yhat)
 
 yhat = np.dot(X, beta)
 print(yhat)
+
+A = np.outer(-beta, x)
+yhat = np.dot(np.transpose(alpha), np.exp(A))
+# :- )
+
+
+yhat = np.sum(alpha[:, None] * np.exp((-beta[:, None]) * x[None, :]), axis = 0)
 
